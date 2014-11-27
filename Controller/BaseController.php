7ff51ab94daad487f82439ac7bb6ae7026d47c5b
hrsalgado <hrsalgado@esei.uvigo.es>
2014-11-27 18:34:01 +0100

    .sql OK

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -2,7 +2,7 @@
 	class BaseController {
 		function ConectarDB()
 		{
-			mysql_connect("localhost","root","")
+			mysql_connect("localhost","usuario","usuario")
 			or die ("Fallo en el establecimiento de la conexión");
 			mysql_select_db("mydb")
 			or die("Error en la selección de la base de datos");
