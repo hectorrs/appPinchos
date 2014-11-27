c4bbb72d9cea8cb449be0b883d49c03cd6e1b6a2
hrsalgado <hrsalgado@esei.uvigo.es>
2014-11-27 18:44:56 +0100

    a

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -1,12 +1,8 @@
 <?php
 	class BaseController {
-		function ConectarDB()
-		{
-<<<<<<< HEAD
-			mysql_connect("localhost","usuario","usuario")
-=======
-			mysql_connect("localhost","root","root")
->>>>>>> origin/master
+		function ConectarDB(){
+			mysql_connect("localhost","usuario","usuario");
+			mysql_connect("localhost","root","root");
 			or die ("Fallo en el establecimiento de la conexión");
 			mysql_select_db("mydb")
 			or die("Error en la selección de la base de datos");
