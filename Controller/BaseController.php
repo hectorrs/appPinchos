192c4f23178fd18effe95b616f2c9bd1e7992ae3
oscar2695 <oscaresei@gmail.com>
2014-11-28 11:22:46 +0100

    Cambios en la base de datos y Base Model-Controller
    Se cambia el sql para añadir un usuario y algunos INSERT necesarios para
    las pruebas.
    Se cambian el Base Controller y el BaseModel para que hagan uso del
    nuevo usuario creado.

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -1,7 +1,7 @@
 <?php
 	class BaseController {
 		function ConectarDB(){
-			mysql_connect("localhost","root","root")
+			mysql_connect("localhost","usuario","usuario")
 			or die ("Fallo en el establecimiento de la conexión");
 			mysql_select_db("mydb")
 			or die("Error en la selección de la base de datos");
