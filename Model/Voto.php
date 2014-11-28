ca68c2d3d963767969c9889707a60ec44720b8e0
oscar2695 <oscaresei@gmail.com>
2014-11-28 11:34:22 +0100

    Se crea la clase voto

---
+++ Model/Voto.php
@@ -0,0 +1,61 @@
+<?php
+	class Voto{
+		private $idVoto;
+		private $puntuacion;
+		private $idPincho;
+		private $idJurado;
+		private $categoria;
+		
+		public function __construct($idVoto=NULL, $puntuacion=NULL, $idPincho=NULL, $idJurado=NULL, $categoria=NULL) {
+			//rellenamos los datos referentes al voto
+			$this->idVoto = $idVoto;
+			$this->puntuacion = $puntuacion;  
+			$this->idPincho = $idPincho;    
+			$this->idJurado = $idJurado;    
+			$this->categoria = $categoria;    
+		}
+		
+		
+		public function getIdVoto() {
+    		return $this->idVoto;
+  		}
+		
+		public function getPuntuacion() {
+    		return $this->puntuacion;
+  		}
+		
+		public function getIdPincho() {
+    		return $this->idPincho;
+  		}
+		
+		public function getIdJurado() {
+    		return $this->idJurado;
+  		}
+		
+		public function getCategoria() {
+    		return $this->categoria;
+  		}
+		
+		
+		public function setIdVoto($idVoto) {
+    		$this->idVoto=$idVoto;
+  		}
+		
+		public function setPuntuacion($puntuacion) {
+    		$this->puntuacion=$puntuacion;
+  		}
+		
+		public function setIdPincho($isPincho) {
+    		$this->idPincho=$idPincho;
+  		}
+		
+		public function setIdJurado($idJurado) {
+    		$this->idJurado=$idJurado;
+  		}
+		
+		public function setCategoria($categoria) {
+    		$this->categoria=$categoria;
+  		}
+		
+	}
+?>
