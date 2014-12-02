763495599acc4c573764ef9ada0460fe1238b8c4
oscar2695 <oscaresei@gmail.com>
2014-12-02 15:39:10 +0100

    Version Final ya enlazada de registrar jurado popular

--- View/Jurados/registrarPopular.php
+++ View/Jurados/registrarPopular.php
@@ -88,7 +88,7 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
 					<li class="active">
-                        <a href="index.php"><i class="fa fa-fw fa-desktop"></i> Home</a>
+                        <a href="../../index.php"><i class="fa fa-fw fa-desktop"></i> Home</a>
                     </li>
 					<li>
                         <a href="#"><i class="fa fa-fw fa-table"></i> Pinchos</a>
